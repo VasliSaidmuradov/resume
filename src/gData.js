a36b4df5c515e0c5668c2aa1e215bc1d7f253535
Vasli Saidmuradov <vasli.said@gmail.com>
2021-01-11 18:02:04 +0600

    refactor gData and add function to count work experience term

--- src/gData.js
+++ src/gData.js
@@ -2,102 +2,102 @@
 
 const gData = {
   goals: [
-    'Ищу команду и место, где смогу максимально раскрыть свой потенциал.',
-    'Развиваться профессионально и получать удовольствия не забывая о балансе.',
-    'Реализовывать интересные и сложные проекты, выдавая отличный результат.',
-    'Активно развиваюсь в своём стеке.',
+    "Ищу команду и место, где смогу максимально раскрыть свой потенциал.",
+    "Развиваться профессионально и получать удовольствия не забывая о балансе.",
+    "Реализовывать интересные и сложные проекты, выдавая отличный результат.",
+    "Активно развиваюсь в своём стеке.",
   ],
   contacts: [
     {
-      icon: 'Envelope',
-      text: 'vasli.said@gmail.com',
-      link: 'mailto:vasli.said@gmail.com',
+      icon: "Envelope",
+      text: "vasli.said@gmail.com",
+      link: "mailto:vasli.said@gmail.com",
     },
     {
-      icon: 'Telephone',
-      text: '+7 701 8009008',
-      link: 'tel:77018009008',
+      icon: "Telephone",
+      text: "+7 701 8009008",
+      link: "tel:77018009008",
     },
     {
-      icon: 'Cursor',
-      text: '@vasli_o',
-      link: 'https://t.me/vasli_o',
+      icon: "Cursor",
+      text: "@vasli_o",
+      link: "https://t.me/vasli_o",
     },
   ],
   aboutMe: {
-    title: '',
+    title: "",
     list: [
-      'Алматы',
-      'Опыт разработки 1.5+ года',
-      {text: 'GitHub', link: 'https://github.com/VasliSaidmuradov'},
-      {text: 'Codewars', link: 'https://www.codewars.com/users/Vasli'},
-    ]
+      "г.Алматы, Казахстан",
+      `Опыт разработки ${getWorkExperienceTerm()}+ года`,
+      { text: "GitHub", link: "https://github.com/VasliSaidmuradov" },
+      { text: "Codewars", link: "https://www.codewars.com/users/Vasli" },
+    ],
   },
   hobby: {
-    title: '',
+    title: "",
     list: [
-      '🏃‍♂️ Бег (Trail running)',
-      '🏓 Настольный теннис',
-      '🏂 Учусь кататься на сноуборде',
-    ]
+      "🏃‍♂️ Бег (Trail running)",
+      "🏓 Настольный теннис",
+      "🏂 Учусь кататься на сноуборде",
+    ],
   },
   skills: {
-    title: '',
+    title: "",
     list: [
-      'JavaScript',
-      'Vue',
-      'Nuxt',
-      'React (изучаю)',
+      "JavaScript",
+      "Vue",
+      "Nuxt",
+      "React (изучаю)",
       // 'Next',
-      'Gatsby (изучаю)',
-      'Git',
-      'Webpack',
-      'HTML/CSS',
-      'Sass/Scss',
-      'Npm/Yarn',
-      'Postman',
-      'Figma',
-      'Trello, Jira',
-      'English B1 (Low Intermediate)',
-    ]
+      "Gatsby (изучаю)",
+      "Git (GitHub, GitLab)",
+      "Webpack",
+      "HTML/CSS",
+      "Sass/Scss",
+      "Npm/Yarn",
+      "Postman",
+      "Figma",
+      "Trello, Jira",
+      "English B1 (Low Intermediate)",
+    ],
   },
   experience: {
-    title: '',
+    title: "",
     list: [
       {
-        title: 'Creative',
-        site: 'https://crtweb.ru/',
-        position: 'Frontend developer',
-        date: 'Октябрь 2020 - по настоящее время',
+        title: "Creative",
+        site: "https://crtweb.ru/",
+        position: "Frontend developer",
+        date: "Октябрь 2020 - по настоящее время",
         list: [
-          'Верстка макетов из figma',
-          'Рефакторинг кода и bugfix',
-          'Разработка компонентов на Vue.js',
-          'Интеграция frontend c backend',
+          "Верстка макетов из figma",
+          "Рефакторинг кода и bugfix",
+          "Разработка компонентов на Vue.js",
+          "Интеграция frontend c backend",
         ],
       },
       {
-        title: 'Brandstudio',
-        site: 'https://brandstudio.kz/',
-        position: 'Frontend developer',
-        date: 'Апрель 2020 - Октябрь 2020',
+        title: "Brandstudio",
+        site: "https://brandstudio.kz/",
+        position: "Frontend developer",
+        date: "Апрель 2020 - Октябрь 2020",
         list: [
-          'Верстка макетов из figma',
-          'Рефакторинг кода и bugfix',
-          'Разработка компонентов на Vue.js(Nuxt)',
-          'Интеграция frontend c backend',
+          "Верстка макетов из figma",
+          "Рефакторинг кода и bugfix",
+          "Разработка компонентов на Vue.js(Nuxt)",
+          "Интеграция frontend c backend",
         ],
       },
       {
-        title: 'Maint',
-        site: 'https://maint.kz/',
-        position: 'Frontend developer',
-        date: 'Март 2019 - Апрель 2020',
+        title: "Maint",
+        site: "https://maint.kz/",
+        position: "Frontend developer",
+        date: "Март 2019 - Апрель 2020",
         list: [
-          'Верстка макетов из photoshop',
-          'Рефакторинг кода и bugfix',
-          'Разработка компонентов на Vue.js(Nuxt)',
-          'Интеграция frontend c backend',
+          "Верстка макетов из photoshop",
+          "Рефакторинг кода и bugfix",
+          "Разработка компонентов на Vue.js(Nuxt)",
+          "Интеграция frontend c backend",
         ],
       },
       // {
@@ -107,20 +107,35 @@ const gData = {
       //   date: '2007 - 2018',
       //   list: [],
       // },
-    ]
+    ],
   },
   education: {
-    title: '',
+    title: "",
     list: [
       {
-        title: 'КазНТУ им К.И. Сатпаева',
-        site: 'https://satbayev.university/ru',
-        position: 'Информационные технологии (компьютерные системы управления)',
-        date: '2001 - 2006',
+        title: "КазНТУ им К.И. Сатпаева",
+        site: "https://satbayev.university/ru",
+        position: "Информационные технологии (компьютерные системы управления)",
+        date: "2001 - 2006",
         list: [],
       },
-    ]
-  }
-}
+    ],
+  },
+};
 
-export default gData
+function getWorkExperienceTerm(startDate = "03.01.2019") {
+  const now = new Date();
+  const start = new Date(startDate);
+
+  let year = now.getFullYear() - start.getFullYear();
+  let month = now.getMonth() - 1 - start.getMonth();
+
+  if (year <= 0 && month < 0) return 0;
+
+  return month < 0
+    ? `${year - 1}.${(year * 12 + month) % 12}`
+    : month === 0
+    ? `${year}`
+    : `${year}.${month}`;
+}
+export default gData;
